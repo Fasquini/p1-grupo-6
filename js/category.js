40a8fb37e8ae890cd30826305aa402c8705a2bc3
Fasquini <fasquini@udesa.edu.ar>
2025-11-08 23:44:40 -0300

    Resolucion parcial del filtrado por categorias

---
+++ js/category.js
@@ -0,0 +1,55 @@
+fetch('https://dummyjson.com/products?limit=194')
+.then(function(response){
+  return response.json();
+})
+.then(function(AllProducts){
+ 
+  let productos = AllProducts.products;
+  console.log(productos);
+
+  
+  let queryString = location.search;
+  console.log(queryString);
+  let queryStringObj = new URLSearchParams(queryString);
+  let Categoria = queryStringObj.get("category");
+  console.log("Categoría elegida:", Categoria);
+
+  let titulo = document.querySelector("section#cat.divisor h1")
+  let destacado = document.querySelector("section#cat.aleatorio")
+  let contenidoCategorias = document.querySelector("section.categoria");
+  titulo.innerHTML = `${Categoria}`;
+  destacado.innerHTML = "";
+  contenidoCategorias.innerHTML = ""; 
+
+  
+  for (i = 0; i < productos.length; i++) {
+    if (Categoria == productos[i].category) {
+      contenidoCategorias.innerHTML += `
+        <article>
+          <img src="${productos[i].thumbnail}" alt="${productos[i].title}">
+          <h2>${productos[i].title}</h2>
+          <p>${productos[i].description}</p>
+          <p><span>Marca:</span> ${productos[i].brand}</p>
+          <p><span>Precio:</span> $${productos[i].price}</p>
+          <p><span>Stock:</span> ${productos[i].stock}</p>
+          <p><span>Rating:</span> ${productos[i].rating}</p>
+          <div class="category">
+            <a href="product.html?id=${productos[i].id}">Ver más</a>
+          </div>
+        </article>
+      `;
+     destacado.innerHTML = `
+     <article>
+     <img src=${productos[i].thumbnail} alt="${productos[i].title}">
+        <h2>${productos[i].title}</h2>
+        <p>${productos[i].description}</p>
+         <article class="precio"><a href= ${productos[i].id}>Ver Más</a> </article>
+    </article>`
+    }
+  }
+
+  
+  if (contenidoCategorias.innerHTML === "") {
+    contenidoCategorias.innerHTML = "<p>No se encontraron productos en esta categoría.</p>";
+  }
+})
--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ let input = document.getElementById('buscador');
 form.addEventListener('submit', function(e) {
   e.preventDefault(); 
 
-  let valor = input.value.trim(); 
+  let valor = input.value; 
 
   if (valor.length === 0) {
     document.querySelector('header.alerta p').textContent = 'Por favor escribí algo antes de buscar ';
@@ -18,7 +18,7 @@ form.addEventListener('submit', function(e) {
     document.querySelector('header.alerta p').textContent = 'Tu búsqueda debe constar de un mínimo de 3 caracteres.';
     document.querySelector('header.alerta').style.display = "flex";
     document.querySelector('header').style.marginBottom = "5px";
-    return; 
+    return;     
   }
 
   else{
@@ -28,3 +28,45 @@ form.addEventListener('submit', function(e) {
 
   }
 });
+
+fetch('https://dummyjson.com/products?limit=194')
+.then(function(response){
+  return response.json();
+})
+.then(function(AllProducts){
+  productos = AllProducts.products
+  console.log(productos)
+  categoria = productos.category
+
+  let contenidoCategorias = document.querySelector("section.categoria");
+contenidoCategorias.innerHTML = "";
+for(i=0;i<productos.length;i++){
+    if(categoria == "mens-watches"){
+  contenidoCategorias.innerHTML += `
+        <article>
+        <article class="reborde">
+          <img src="${productos[i].thumbnail}" alt="${productos[i].title}">
+        </article>
+          <h2>${productos[i].title}</h2>
+          <p>${productos[i].description}</p>
+          <p><span>Marca:</span> ${productos[i].brand}</p>
+          <p><span>Precio:</span> $${productos[i].price}</p>
+          <p><span>Stock:</span> ${productos[i].stock}</p>
+          <p><span>Rating:</span> ${productos[i].rating}</p>
+          <div class="category">
+            <a href="product.html?id=${productos[i].id}">Ver más</a>
+          </div>
+        </article>
+      `;
+    }
+    else{
+        
+    }
+        
+    }
+  });
+
+
+
+
+   
