09a1b467f5fb4a68d6aaa51b9059f0a20b3b8083
Lorenzo Ruberto <lorenzoruberto@MacBook-Pro.local>
2025-11-14 20:39:39 -0300

    validacion y guardado de datos en el storage

---
+++ js/login.js
@@ -0,0 +1,44 @@
+let formulario = document.querySelector('#log form');
+
+let inputEmail = document.querySelector('input[name="mailUsuario"]');
+let inputContrasena = document.querySelector('input[name="contraseñaUsuario"]');
+
+let mensajes = document.querySelectorAll('#log p');
+let emailError = mensajes[0];
+let contrasenaError = mensajes[1];
+
+formulario.addEventListener('submit', function(e){
+
+  e.preventDefault();
+
+  emailError.innerText = "";
+  contrasenaError.innerText = "";
+
+  let valorEmail = inputEmail.value;
+  let valorContrasena = inputContrasena.value;
+
+  let cantidadErrores = 0;
+
+  if (valorEmail === ""){
+    emailError.innerText = "El email es obligatorio";
+    cantidadErrores = cantidadErrores + 1;
+  }
+
+  if (valorContrasena === ""){
+    contrasenaError.innerText = "La contraseña es obligatoria";
+    cantidadErrores = cantidadErrores + 1;
+  } else{
+
+    if (valorContrasena.length < 6){
+      contrasenaError.innerText = "La contraseña debe tener al menos 6 caracteres";
+      cantidadErrores = cantidadErrores + 1;
+    }
+  }
+
+  if (cantidadErrores === 0){
+
+    localStorage.setItem('emailUsuario', valorEmail);
+
+    window.location.href = 'index.html';
+  }
+});
