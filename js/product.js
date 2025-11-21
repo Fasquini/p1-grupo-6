035dbdc371368d0299f764be3d2db9115cfca462
Lorenzo Ruberto <lorenzoruberto@MacBook-Pro.local>
2025-11-21 11:33:51 -0300

    arreglos de js

--- js/product.js
+++ js/product.js
@@ -109,3 +109,57 @@ for(i=0;i<productos.length;i++){
 pestana.innerHTML = `Booked™ | ${nombre}`
 }})
 
+fetch('https://dummyjson.com/products/search?q='+ Busqueda)
+.then(function(response){
+  return response.json();
+})
+.then(function(AllProducts){
+ 
+  let productos = AllProducts.products;
+  console.log(productos);
+
+  let hay = document.querySelector('section#hay.divisor');
+    let msj = document.querySelector('section#hay.divisor h2')
+  let noHay = document.querySelector('section#hay.divisor');
+  msj.innerHTML = `No se encontraron resultados para `
+  msj.style.display = "block"
+  noHay.style.display = "block"
+  let contenidoBusqueda= document.querySelector("section.contenido");
+if (contenidoBusqueda.innerHTML === "") {
+  noHay.style.display = "block"
+}
+else{
+  for (i = 0; i < productos.length; i++) {
+      contenidoBusqueda.innerHTML += `
+        <article id="bu">
+        <img src="${productos[i].thumbnail}" alt="${productos[i].title}">
+        <h2>${productos[i].title}</h2>
+        <p>${productos[i].description}</p>
+        <a href="product.html?category=${productos[i].category}&name=${productos[i].title}&id=${productos[i].id}">Ver Más</a>
+
+    </article> `;
+  msj.innerHTML = `Se muestran resultados para `
+      ;
+  }
+  }
+    
+msj.textContent += ` "${Busqueda}"`
+
+pestana.innerHTML = `Booked™ | "${Busqueda}"`
+let links = document.querySelectorAll("#bu a");
+  
+    for (i = 0; i < links.length; i++) {
+    links[i].addEventListener("mouseover", function() {
+      this.style.color = "rgba(231, 177, 0, 0.8)";
+      this.style.fontSize = "16px";
+      this.style.fontWeight = "700";
+      
+    });
+    links[i].addEventListener("mouseout", function() {
+      this.style.color = "";
+      this.style.fontSize = "";
+      this.style.fontWeight = "";
+    });
+  }
+
+})
